c5e15f5233dd27b0cfd7e84b5d622b7c441a9b1a
Tony Esterdark <messi.klate@gmail.com>
2016-07-19 00:42:19 +0800

    Update UnlimitedUbuntu.sh

--- UnlimitedUbuntu.sh
+++ UnlimitedUbuntu.sh
@@ -19,7 +19,7 @@ service pritunl start
 # Install Squid
 apt-get -y install squid3
 cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/syahmi86/VPN/master/squid.conf" 
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/syahmi86/VPN/master/conf/squid.conf" 
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
 sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
 service squid3 restart
@@ -34,11 +34,11 @@ apt-get -y install nginx php5-fpm php5-cli
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/syahmi86/VPN/master/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/syahmi86/VPN/master/conf/nginx.conf"
 mkdir -p /home/vps/public_html
 echo "<pre>Setup by Syahmi</pre>" > /home/vps/public_html/index.html
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/syahmi86/VPN/master/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/syahmi86/VPN/master/conf/vps.conf"
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 service php5-fpm restart
 service nginx restart
