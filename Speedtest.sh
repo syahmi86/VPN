5aa077fe90b8c1ef55b14a27829ce08898d5f953
Tony Esterdark <messi.klate@gmail.com>
2016-10-22 16:27:08 +0800

    Update Speedtest.sh

--- Speedtest.sh
+++ Speedtest.sh
@@ -1,6 +1,6 @@
 # Install Menu for OpenVPN
 cd
-wget https://raw.githubusercontent.com/syahz86/VPN/master/conf/Speedtest
+wget https://raw.githubusercontent.com/syahz86/VPN/master/conf/speedtest_cli.py
 chmod a+rx speedtest_cli.py
 sudo mv speedtest_cli.py /usr/local/bin/speedtest-cli
 sudo chown root:root /usr/local/bin/speedtest-cli
