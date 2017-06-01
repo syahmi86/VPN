0b4b4d43d4318128c785a5f4324b2e3c0d87e0bd
Tony Esterdark <messi.klate@gmail.com>
2017-06-01 15:06:17 +0800

    Update CustomDebian-kvm.sh

--- CustomDebian-kvm.sh
+++ CustomDebian-kvm.sh
@@ -28,9 +28,6 @@ apt-get -y --purge remove bind9*;
 apt-get update 
 apt-get -y upgrade
 
-# install webserver
-apt-get -y install nginx php5-fpm php5-cli
-
 # install essential package
 apt-get -y install nmap nano iptables sysv-rc-conf openvpn vnstat apt-file
 apt-get -y install libexpat1-dev libxml-parser-perl
@@ -55,51 +52,6 @@ mv screenfetch-dev /usr/bin/screenfetch
 chmod +x /usr/bin/screenfetch
 echo "clear" >> .profile
 echo "screenfetch" >> .profile
-
-# Install Web Server
-cd
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/muchigo/VPS/master/conf/nginx.conf"
-mkdir -p /home/vps/public_html
-echo "<pre>Setup by SiTony</pre>" > /home/vps/public_html/index.html
-echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/muchigo/VPS/master/conf/vps.conf"
-sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
-service php5-fpm restart
-service nginx restart
-
-# install openvpn
-wget -O /etc/openvpn/openvpn.tar "https://raw.githubusercontent.com/muchigo/VPS/master/conf/openvpn.tar"
-cd /etc/openvpn/
-tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/muchigo/VPS/master/conf/1194-debian.conf"
-service openvpn restart
-sysctl -w net.ipv4.ip_forward=1
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.up.rules "https://raw.githubusercontent.com/muchigo/VPS/master/conf/iptables.up.rules"
-sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-sed -i 's/port 1194/port 6500/g' /etc/openvpn/1194.conf
-sed -i 's/proto tcp/proto udp/g' /etc/openvpn/1194.conf
-sed -i $MYIP2 /etc/iptables.up.rules;
-iptables-restore < /etc/iptables.up.rules
-service openvpn restart
-
-# configure openvpn client config
-cd /etc/openvpn/
-wget -O /etc/openvpn/1194-client.ovpn "https://raw.githubusercontent.com/muchigo/VPS/master/conf/1194-client.conf"
-sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
-sed -i 's/proto tcp/proto udp/g' /etc/openvpn/1194-client.ovpn
-sed -i 's/1194/6500/g' /etc/openvpn/1194-client.ovpn
-NAME=`uname -n`.`awk '/^domain/ {print $2}' /etc/resolv.conf`;
-mv /etc/openvpn/1194-client.ovpn /etc/openvpn/$NAME.ovpn
-useradd -M -s /bin/false test1
-echo "test1:test1" | chpasswd
-tar cf client.tar $NAME.ovpn
-cp client.tar /home/vps/public_html/
-cd
 
 # setting port ssh
 sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
