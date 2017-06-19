6a2f0182bda3fc41a2d911749192dd32d0378a08
Tony Esterdark <messi.klate@gmail.com>
2017-06-19 18:37:32 +0800

    Update Centos-kvm.sh

--- Centos-kvm.sh
+++ Centos-kvm.sh
@@ -84,19 +84,17 @@ wget -O /etc/openvpn/openvpn.tar "https://raw.githubusercontent.com/syahz86/VPN/
 cd /etc/openvpn/
 tar xf openvpn.tar
 wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/syahz86/VPN/master/conf/1194-centos.conf"
-wget -O /etc/iptables.up.rules "https://raw.githubusercontent.com/syahz86/VPN/master/conf/iptables.up.rules"
+service openvpn restart
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+wget -O /etc/iptables.up.rules "https://raw.githubusercontent.com/syahz86/VPS/master/conf/iptables.up.rules"
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
-sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.d/rc.local
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
+sed -i 's/port 1194/port 6500/g' /etc/openvpn/1194.conf
 sed -i $MYIP2 /etc/iptables.up.rules;
-sed -i 's/venet0/eth0/g' /etc/iptables.up.rules
 iptables-restore < /etc/iptables.up.rules
-sysctl -w net.ipv4.ip_forward=1
-sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
 service openvpn restart
-chkconfig openvpn on
-cd
 
 # configure openvpn client config
 cd /etc/openvpn/
