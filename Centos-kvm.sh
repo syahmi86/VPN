97b4ba29959bcb720ee118cf00739c5e606b6320
Tony Esterdark <messi.klate@gmail.com>
2017-06-19 11:25:29 +0800

    Update Centos-kvm.sh

--- Centos-kvm.sh
+++ Centos-kvm.sh
@@ -179,6 +179,66 @@ cd
 wget https://raw.githubusercontent.com/syahz86/VPN/master/conf/userlogin.sh
 chmod +x userlogin.sh
 
+#bonus block torrent
+iptables -A INPUT -m string --algo bm --string "BitTorrent" -j REJECT
+iptables -A INPUT -m string --algo bm --string "BitTorrent protocol" -j REJECT
+iptables -A INPUT -m string --algo bm --string "peer_id=" -j REJECT
+iptables -A INPUT -m string --algo bm --string ".torrent" -j REJECT
+iptables -A INPUT -m string --algo bm --string "announce.php?passkey=" -j REJECT
+iptables -A INPUT -m string --algo bm --string "torrent" -j REJECT
+iptables -A INPUT -m string --algo bm --string "info_hash" -j REJECT
+iptables -A INPUT -m string --algo bm --string "/default.ida?" -j REJECT
+iptables -A INPUT -m string --algo bm --string ".exe?/c+dir" -j REJECT
+iptables -A INPUT -m string --algo bm --string ".exe?/c_tftp" -j REJECT
+iptables -A INPUT -m string --string "peer_id" --algo kmp -j REJECT
+iptables -A INPUT -m string --string "BitTorrent" --algo kmp -j REJECT
+iptables -A INPUT -m string --string "BitTorrent protocol" --algo kmp -j REJECT
+iptables -A INPUT -m string --string "bittorrent-announce" --algo kmp -j REJECT
+iptables -A INPUT -m string --string "announce.php?passkey=" --algo kmp -j REJECT
+iptables -A INPUT -m string --string "find_node" --algo kmp -j REJECT
+iptables -A INPUT -m string --string "info_hash" --algo kmp -j REJECT
+iptables -A INPUT -m string --string "get_peers" --algo kmp -j REJECT
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j REJECT
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j REJECT
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j REJECT
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j REJECT
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j REJECT
+iptables -A FORWARD -m string --algo bm --string "torrent" -j REJECT
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j REJECT
+iptables -A FORWARD -m string --algo bm --string "/default.ida?" -j REJECT
+iptables -A FORWARD -m string --algo bm --string ".exe?/c+dir" -j REJECT
+iptables -A FORWARD -m string --algo bm --string ".exe?/c_tftp" -j REJECT
+iptables -A FORWARD -m string --string "peer_id" --algo kmp -j REJECT
+iptables -A FORWARD -m string --string "BitTorrent" --algo kmp -j REJECT
+iptables -A FORWARD -m string --string "BitTorrent protocol" --algo kmp -j REJECT
+iptables -A FORWARD -m string --string "bittorrent-announce" --algo kmp -j REJECT
+iptables -A FORWARD -m string --string "announce.php?passkey=" --algo kmp -j REJECT
+iptables -A FORWARD -m string --string "find_node" --algo kmp -j REJECT
+iptables -A FORWARD -m string --string "info_hash" --algo kmp -j REJECT
+iptables -A FORWARD -m string --string "get_peers" --algo kmp -j REJECT
+iptables -A OUTPUT -m string --algo bm --string "BitTorrent" -j REJECT
+iptables -A OUTPUT -m string --algo bm --string "BitTorrent protocol" -j REJECT
+iptables -A OUTPUT -m string --algo bm --string "peer_id=" -j REJECT
+iptables -A OUTPUT -m string --algo bm --string ".torrent" -j REJECT
+iptables -A OUTPUT -m string --algo bm --string "announce.php?passkey=" -j REJECT
+iptables -A OUTPUT -m string --algo bm --string "torrent" -j REJECT
+iptables -A OUTPUT -m string --algo bm --string "info_hash" -j REJECT
+iptables -A OUTPUT -m string --algo bm --string "/default.ida?" -j REJECT
+iptables -A OUTPUT -m string --algo bm --string ".exe?/c+dir" -j REJECT
+iptables -A OUTPUT -m string --algo bm --string ".exe?/c_tftp" -j REJECT
+iptables -A OUTPUT -m string --string "peer_id" --algo kmp -j REJECT
+iptables -A OUTPUT -m string --string "BitTorrent" --algo kmp -j REJECT
+iptables -A OUTPUT -m string --string "BitTorrent protocol" --algo kmp -j REJECT
+iptables -A OUTPUT -m string --string "bittorrent-announce" --algo kmp -j REJECT
+iptables -A OUTPUT -m string --string "announce.php?passkey=" --algo kmp -j REJECT
+iptables -A OUTPUT -m string --string "find_node" --algo kmp -j REJECT
+iptables -A OUTPUT -m string --string "info_hash" --algo kmp -j REJECT
+iptables -A OUTPUT -m string --string "get_peers" --algo kmp -j REJECT
+iptables -A INPUT -p tcp --dport 25 -j REJECT   
+iptables -A FORWARD -p tcp --dport 25 -j REJECT 
+iptables -A OUTPUT -p tcp --dport 25 -j REJECT 
+iptables-save
+
 # set time GMT +8
 ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
 
@@ -211,6 +271,7 @@ echo "vnstat   : http://$MYIP:81/vnstat/"
 echo "Timezone : Asia/Malaysia"
 echo "Fail2Ban : [on]"
 echo "IPv6     : [off]"
+echo "Torrent Block  : [on]"
 echo "Dropbear : please type sh userlogin.sh port to check login user"
 echo "Please Reboot your VPS !"
 echo ""
