5b335f6d008bb3462c67df712296fa0fd86eeebf
Tony Esterdark <messi.klate@gmail.com>
2017-06-22 22:20:03 +0800

    Update userlogin.sh

--- conf/userlogin.sh
+++ conf/userlogin.sh
@@ -1,24 +1,52 @@
-#!/bin/bash
-#
-#
-# ========================
-#
-
-data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
-
-echo "Checking user login";
-echo "---";
-
-for PID in "${data[@]}"
+#!/usr/bin
+if [ $1 ];then
+port_dropbear=$1
+log=/var/log/auth.log
+loginsukses='Password auth succeeded'
+echo ' '
+echo ' '
+echo "               Dropbear Users Login Monitor "
+echo "---------------------------------------------------------------"
+echo "  Date-time    |  PID      |  User Name      |  From Host " 
+echo "---------------------------------------------------------------" 
+pids=`ps ax |grep dropbear |grep  " $port_dropbear" |awk -F" " '{print $1}'`
+for pid in $pids 
 do
-#echo "check $PID";
-NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep $PID | wc -l`;
-USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep $PID | awk '{print $10}'`;
-IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep $PID | awk '{print $12}'`;
-if [ $NUM -eq 1 ]; then
-echo "$PID - $USER - $IP";
-fi
-done
+    pidlogs=`grep $pid $log |grep "$loginsukses" |awk -F" " '{print $3}'`
+    i=0
+    for pidend in $pidlogs
+    do
+      let i=i+1
+    done   
+    if [ $pidend ];then
+       login=`grep $pid $log |grep "$pidend" |grep "$loginsukses"`
+       PID=$pid
+       user=`echo $login |awk -F" " '{print $10}' | sed -r "s/'/ /g"`
+       waktu=`echo $login |awk -F" " '{print $2,$3}'`
+       while [ ${#waktu} -lt 13 ]
+       do
+           waktu=$waktu" " 
+       done
 
-echo "---";
-echo "---";
+       while [ ${#user} -lt 16 ]
+       do
+           user=$user" " 
+       done
+       while [ ${#PID} -lt 8 ]
+       do
+           PID=$PID" " 
+       done
+
+       fromip=`echo $login |awk -F" " '{print $12}' |awk -F":" '{print $1}'`
+       echo "  $waktu|  $PID | $user|  $fromip "
+    fi
+done
+echo "---------------------------------------------------------------" 
+echo "                        Script Modified by GollumVPN   " 
+else
+echo "Gunakan perintah sh dropmon.sh [port]"
+echo "contoh : sh dropmon.sh 443"
+echo ' '
+echo ' '
+fi
+exit 0
